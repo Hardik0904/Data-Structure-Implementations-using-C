ce0016da76442da5a53e58b6d5d34c01937304c0
Hardik Shah <hardikshahrs@gmail.com>
2026-08-13 19:45:26 +0530

    fixed some bugs: innit resize push_back and print works fine

--- Dynamic_arr.c
+++ Dynamic_arr.c
@@ -8,7 +8,7 @@ typedef struct{
     size_t capacity;
 } DynamicArray;
 
-//Initialize the ynamic array
+//Initialize the dynamic array
 DynamicArray* da_init(void){
     DynamicArray *da = (DynamicArray *)malloc(sizeof(DynamicArray));
     if(!da){
@@ -28,7 +28,7 @@ DynamicArray* da_init(void){
 
 //internal fuction to resize the capacity of array
 static void da_resize(DynamicArray* da, size_t new_capacity){
-    int* new_data=(int*)realloc(new_capacity*sizeof(int));
+    int* new_data=(int*)realloc(da->data, new_capacity*sizeof(int));
     if(!new_data){
         perror("failed to reallocate new buffer");
         return;
@@ -37,14 +37,30 @@ static void da_resize(DynamicArray* da, size_t new_capacity){
     da->capacity = new_capacity;
 }
 
+//push item to back 
 void da_push_back(DynamicArray *da, int val){
     if(da->size == da->capacity){
         da_resize(da, da->capacity*2);
-        da->data[da->size++]=val;
     }
+    da->data[da->size++]=val;;
 }
 
 
+//print arary elements
+void da_print(DynamicArray *da){
+    printf("[");
+    for(int i=0; i<da->size; i++){
+        printf("%d", da->data[i]);
+        if(i < da->size - 1)
+            printf(", ");
+    }
+    printf("]");
+}
 int main(){
-
+    DynamicArray *array = da_init();
+    da_push_back(array, 4);
+    da_push_back(array, 7);
+    da_push_back(array, 45);
+    da_push_back(array, 9);
+    da_print(array);
 }
