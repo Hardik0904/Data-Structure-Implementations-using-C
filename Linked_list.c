9f789a5b6196cc4e601df7a041ea868e910b40db
Hardik Shah <hardikshahrs@gmail.com>
2026-08-19 02:30:49 +0530

    added the insert at k function in ll

--- Linked_list.c
+++ Linked_list.c
@@ -25,31 +25,50 @@ node* ll_init(int value){
 }
 
 //push element at back
-void push_back(node **head, int value){
+void push_back(node *head, int value){
     
     node* new_node=ll_init(value);
-    if(*head==NULL){
-        *head=new_node;
+    if(head==NULL){
+        head=new_node;
     }
-    node* temp=*head;
+    node* temp=head;
     while(temp->next!=NULL){
         temp=temp->next;
     }
     temp->next=new_node;
 }
 
+//insert at k
+node* insert_k(node* head, int k, int value){
+    node* new_node=ll_init(value);
+    if(k==1){
+        new_node->next=head;
+        head=new_node;
+        return head;
+    }
+    node* temp=head;
+    int cnt=0;
+    for(int i=0; i<k-2; i++){
+        temp=temp->next;
+    }
+    new_node->next=temp->next;
+    temp->next=new_node;
+    return head;
+}
+
 //print ll
 void print_ll(node* head){
     node* temp=head;
     while(temp!=NULL){
-        printf("%d", *temp->data);
+        printf("%d ", *temp->data);
         temp=temp->next;
     }
 }
 
 int main(){
     node* newnode=ll_init(3);
-    push_back(&newnode, 6);
-    push_back(&newnode, 10);
-    print_ll(newnode);
+    push_back(newnode, 6);
+    push_back(newnode, 10);
+    node* head=insert_k(newnode, 1, 12);
+    print_ll(head);
 }
